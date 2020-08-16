e0e91f18a377994a91ebe05a336daeb3351ff60b
Christopher Scully <scullycphone@gmail.com>
2020-08-15 21:08:50 -0500

    update for models

--- startsimulation.sh
+++ startsimulation.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 export SVGA_VGPU10=0
-export GAZEBO_RESOURCE_PATH=~/Documents/gazebo_models-master
-export GAZEBO_MODEL_DATABASE_URI=http://gazebosim.org/models
-export GAZEBO_MASTER_IP=localhost
-export GAZEBO_MASTER_URI=localhost:11345
 gzclient --verbose
